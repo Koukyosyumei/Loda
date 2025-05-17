ea2f7d6de41b7f902df6ef694489875abdbc4d51
Koukyosyumei <koukyosyumei@hotmail.com>
2025-05-17 16:37:27 -0400

    update

--- Loda/Field.lean
+++ Loda/Field.lean
@@ -22,66 +22,12 @@ instance (p : ℕ) : CommRing (F p) := ZMod.commRing p
 instance (p : ℕ) [Fact p.Prime] : Repr (F p) where
   reprPrec x _ := "F" ++ toString p ++ ".mk " ++ toString x.val
 
-variable {p : ℕ} [Fact p.Prime]
-
-/-- Coercion from `Fin p` to `Fp p`. -/
-instance : Coe (Fin p) (F p) where
-  coe x := x
-
-/-- Zero element of `Fp p`. -/
-def zero : F p := 0
-instance : Zero (F p) where
-  zero := zero
-
-/-- One element of `Fp p`. -/
-def one: F p := 1
-instance : One (F p) where
-  one := one
-
-/-- Addition in `Fp p`. -/
-def mod_add (a b : F p) : F p := (a.val + b.val) % p
-instance : Add (F p) where
-  add := mod_add
-
-/-- Negation in `Fp p`. -/
-def neg (a : F p) : F p := (p - a.val) % p
-instance : Neg (F p) where
-  neg := neg
-
-/-- Multiplication in `Fp p`. -/
-@[simp]
-def mod_mul (a b : F p) : F p := (a.val * b.val) % p
-instance : Mul (F p) where
-  mul := mod_mul
-instance : HMul (F p) (F p) (F p) where
-  hMul := fun a b => mod_mul a b
-
-@[simp] lemma mod_mul_eq_mul (a b : F p) : mod_mul a b = a * b := rfl
-
-/-- Exponentiation by natural number in `Fp p`. -/
-instance : Pow (F p) ℕ where
-  pow a n := a ^ n  -- this just defers to ZMod's own `^`
-
-/-- Inverse in `Fp p` using Fermat's little theorem (`a^(p-2)`). -/
-instance : Inv (F p) where
-  inv a := a ^ (p - 2)
-
--- First, correctly implement division in Fp
-instance : Div (F p) where
-  div a b := a * (b ^ (p - 2))
-
--- Then implement HDiv with the correct signature
-instance : HDiv (F p) (F p) (F p) where
-  hDiv := fun a b => a * (b ^ (p - 2))
-
 def a : F 5 := 2
 def b : F 5 := 3
-
-set_option diagnostics true
 
 #eval (a * b)                 -- Expected: 1
 #eval ((2 : F 5) + (3 : F 5)) -- Expected 0
 #eval (a^2)
 #eval (a^3)
 #eval (a^4)
---#eval (a * (b ^ (5 - 2)))
+#eval (a * b.inv)
