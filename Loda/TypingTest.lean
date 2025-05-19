a30312122d031245d9e35e38eef4f6b024835329
Koukyosyumei <koukyosyumei@hotmail.com>
2025-05-19 08:51:58 -0400

    update

--- Loda/TypingTest.lean
+++ Loda/TypingTest.lean
@@ -11,29 +11,29 @@ def σ0 : Env.ValEnv := fun x =>
   | _ => Value.vStar
 def Γ0 : Env.TyEnv := fun x =>
   match x with
-  | "x" => (Ty.refin Ty.bool (Ast.Expr.constBool true))
-  | "b" => (Ty.refin Ty.bool (Ast.Expr.constBool true))
+  | "x" => (Ty.refin Ty.bool (Expr.constBool true))
+  | "b" => (Ty.refin Ty.bool (Expr.constBool true))
   | "y" => Ty.int
   | _ => Ty.unit
 def Γ1 := Env.setTy Γ0 "x" Ty.bool
 def δ0 : Env.CircuitEnv :=
-  fun _ => { name := "idInt", inputs := [("x", Ast.Ty.int)], output := ("out", Ast.Ty.int),
-                 body := Ast.Expr.var "x" }
+  fun _ => { name := "idInt", inputs := [("x", Ty.int)], output := ("out", Ty.int),
+                 body := Expr.var "x" }
 
 example : Ty.SubtypeJudgment σ0 δ0 Γ0 123 (pure Ty.int) (pure Ty.int) :=
   Ty.SubtypeJudgment.TSub_Refl
 
 -- refinement subtyping: {v:int | True} <: {v:int | True}
 example : Ty.SubtypeJudgment σ0 δ0 Γ0 123
-  (pure (Ty.refin Ty.int (Ast.Expr.constBool true)))
-  (pure (Ty.refin Ty.int (Ast.Expr.constBool true))) :=
+  (pure (Ty.refin Ty.int (Expr.constBool true)))
+  (pure (Ty.refin Ty.int (Expr.constBool true))) :=
   Ty.SubtypeJudgment.TSub_Refl
 
 -- refinement subtyping: {v:int | y + y} <: {v:int | 2 * y}
--- (Ast.Expr.intExpr (Ast.Expr.constInt 2) Ast.IntegerOp.mul (Ast.Expr.var "y"))
+-- (Expr.intExpr (Expr.constInt 2) IntegerOp.mul (Expr.var "y"))
 example (y: ℕ) (hσy : σ0 "y" = Value.vInt y) : Ty.SubtypeJudgment σ0 δ0 Γ0 123
-  (pure (Ty.refin Ty.int (expr_eq v (Ast.Expr.intExpr (Ast.Expr.var "y") Ast.IntegerOp.add (Ast.Expr.var "y")))))
-  (pure (Ty.refin Ty.int (expr_eq v (Ast.Expr.intExpr (Ast.Expr.constInt 2) Ast.IntegerOp.mul (Ast.Expr.var "y")))))
+  (pure (Ty.refin Ty.int (expr_eq v (Expr.intExpr (Expr.var "y") IntegerOp.add (Expr.var "y")))))
+  (pure (Ty.refin Ty.int (expr_eq v (Expr.intExpr (Expr.constInt 2) IntegerOp.mul (Expr.var "y")))))
   := by
   apply Ty.SubtypeJudgment.TSub_Refine
   · apply Ty.SubtypeJudgment.TSub_Refl
@@ -56,7 +56,7 @@ example (y: ℕ) (hσy : σ0 "y" = Value.vInt y) : Ty.SubtypeJudgment σ0 δ0 Γ
   rw[two_mul]
 
 -- TE_VAR: assume env maps "b" to {v | v = eval ...}
-example : Ty.TypeJudgment σ0 δ0 Γ0 123 (Expr.var "b") ((Ty.refin Ty.bool (expr_eq v (Ast.Expr.var "b"))), σ0) := by
+example : Ty.TypeJudgment σ0 δ0 Γ0 123 (Expr.var "b") ((Ty.refin Ty.bool (expr_eq v (Expr.var "b"))), σ0) := by
   apply Ty.TypeJudgment.TE_Var
   simp [Γ0]
   rfl
