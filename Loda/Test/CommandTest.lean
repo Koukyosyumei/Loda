85898485c68bebdecebffd27f72e91e9459e110d
Koukyosyumei <koukyosyumei@hotmail.com>
2025-06-11 16:04:07 -0400

    update

--- Loda/Test/CommandTest.lean
+++ Loda/Test/CommandTest.lean
@@ -17,48 +17,11 @@ open Env
   intro x hs hσ
   set σ := (Env.updateVal [] "x" x)
   set Γ := (Env.updateTy (fun x ↦ Ty.unit) "x" (Ty.int.refin (Expr.constBool True)))
-  have h_body :
-    Ty.TypeJudgment
-      Γ
-      (Ast.Expr.letIn "out"
-         (Ast.Expr.intExpr (Ast.Expr.var "x") Ast.IntegerOp.add (Ast.Expr.var "x"))
-         (Ast.Expr.var "out"))
-      (Ast.Ty.refin Ast.Ty.int
-         (Ast.exprEq Ast.v (Ast.Expr.intExpr (Ast.Expr.var "x") Ast.IntegerOp.add (Ast.Expr.var "x")))) := by {
-          apply @let_binding_int_op_type_preservation 1000 "x" "x" "out" σ Δ Γ
-          simp [Γ]
-          rfl
-          simp [Γ]
-          rfl
-         }
-  unfold PropSemantics.tyenvToProp at hσ
-  simp [Γ] at hσ
-  have hσ₁ : Env.lookupVal σ "x" = x := by {
-    simp [σ]
-    rfl
-  }
-  have h : ∃ (a : ℤ), Env.lookupVal σ "x" = Ast.Value.vInt a := by
-    cases x with
-    | vInt n =>
-      use n
-    | _ =>
-      simp [hσ₁] at hσ
-      simp_all
-      cases hσ with
-      | intro left right => {
-        contradiction
-      }
-  obtain ⟨vv, hv_eq⟩ := h
-  have h_sub :
-    @Ty.SubtypeJudgment 1000 σ Δ Γ
-      (some (Ast.Ty.refin Ast.Ty.int (Ast.exprEq Ast.v (Ast.Expr.intExpr (Ast.Expr.var "x") Ast.IntegerOp.add (Ast.Expr.var "x")))))
-      (some (Ast.Ty.refin Ast.Ty.int (Ast.exprEq Ast.v (Ast.Expr.intExpr (Ast.Expr.constInt 2) Ast.IntegerOp.mul (Ast.Expr.var "x"))))) := by {
-        apply two_mul_I 1000 σ Δ Γ "x" vv
-        simp [σ]
-        simp_all
-        rfl
-      }
-  unfold Ast.exprEq at h_sub
+  have hΓ : Γ "x" = (Ty.int.refin (Expr.constBool True)) := rfl
+  have h_body := @let_binding_int_op_type_preservation 1000 "x" "x" "out" σ Δ Γ
+              Ast.IntegerOp.add (Ast.Expr.constBool True) (Ast.Expr.constBool True) hΓ hΓ
+  obtain ⟨vv, hv_eq⟩ := int_refintype_implies_exists_int_value 1000 σ Δ Γ "x" (Expr.constBool True) hΓ hσ
+  have h_sub := two_mul_I 1000 σ Δ Γ "x" vv hv_eq
   simp [h_delta, Γ, Ast.v] at h_sub
   simp [h_delta, Ast.v] at h_body
   exact Ty.TypeJudgment.TE_SUB h_sub h_body
