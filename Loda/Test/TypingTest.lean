8aead463fdc2472dcff0c430b17c950b7fe5cc49
Koukyosyumei <koukyosyumei@hotmail.com>
2025-06-11 15:41:01 -0400

    update

--- Loda/Test/TypingTest.lean
+++ Loda/Test/TypingTest.lean
@@ -81,7 +81,7 @@ def mulCircuit : Ast.Circuit := {
 }
 def Δ : Env.CircuitEnv := [("mul", mulCircuit)]
 
-lemma intRefinTypeImpliesExistsIntValue (fuel: ℕ) (σ: Env.ValEnv) (Δ: Env.CircuitEnv) (Γ: Env.TyEnv) (x: String) (e: Expr)
+lemma int_refintype_implies_exists_int_value (fuel: ℕ) (σ: Env.ValEnv) (Δ: Env.CircuitEnv) (Γ: Env.TyEnv) (x: String) (e: Expr)
   : (Γ x = Ty.int.refin e) → PropSemantics.tyenvToProp fuel σ Δ Γ x → ∃ (a: ℤ), Env.lookupVal σ x = Ast.Value.vInt a := by
   intro hx
   unfold PropSemantics.tyenvToProp
@@ -101,7 +101,7 @@ theorem mulCircuit_correct : (Ty.circuitCorrect 1000 Δ mulCircuit) := by
   have hΓ : Γ "x" = (Ty.int.refin (Expr.constBool true)) := rfl
   have h_body := @let_binding_int_op_type_preservation 1000 "x" "x" "out" σ Δ Γ
               Ast.IntegerOp.add (Ast.Expr.constBool true) (Ast.Expr.constBool true) hΓ hΓ
-  obtain ⟨vv, hv_eq⟩ := intRefinTypeImpliesExistsIntValue 1000 σ Δ Γ "x" (Expr.constBool true) hΓ hσ
+  obtain ⟨vv, hv_eq⟩ := int_refintype_implies_exists_int_value 1000 σ Δ Γ "x" (Expr.constBool true) hΓ hσ
   have h_sub := two_mul_I 1000 σ Δ Γ "x" vv hv_eq
   exact Ty.TypeJudgment.TE_SUB h_sub h_body
 
