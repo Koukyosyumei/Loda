ba19e871a2d09cd36e254e087fd25c36fc5e1199
Koukyosyumei <koukyosyumei@hotmail.com>
2025-06-12 09:14:12 -0400

    update

--- Loda/Command.lean
+++ Loda/Command.lean
@@ -77,7 +77,9 @@ unsafe def elabLodaProve : Elab.Command.CommandElab
 
     -- Generate the theorem syntax
     let theoremStx ← `(command|
-      theorem $theoremIdent (Δ: Env.CircuitEnv) (h_delta: Δ = $deltaTerm) : (Ty.circuitCorrect 1000 $deltaTerm $circTerm) := by $proof
+      theorem $theoremIdent (Δ: Env.CircuitEnv) (h_delta: Δ = $deltaTerm) : (Ty.circuitCorrect 1000 $deltaTerm $circTerm) := by
+        (unfold Ty.circuitCorrect; simp_all; intro x hs hσ; set envs := Ty.makeEnvs $circTerm x);
+        (set σ := envs.1); (set Γ := envs.2); ($proof);
     )
     logInfo m!"Proof state opened for '{theoremName}' - continue with tactics!"
     -- Elaborate the generated theorem command
--- Loda/Test/CommandTest.lean
+++ Loda/Test/CommandTest.lean
@@ -12,13 +12,7 @@ open Env
 #loda_eval Adder x=2
 
 #loda_prove Adder := by {
-  rename_i Δ h_delta
-  unfold Ty.circuitCorrect
-  simp_all
-  intro x hs hσ
-  set envs := Ty.makeEnvs mulCircuit x
-  set σ := envs.1
-  set Γ := envs.2
+  rename_i Δ h_delta x hs envs σ Γ hσ
   have hΓ : Γ "x" = (Ty.int.refin (Expr.constBool True)) := rfl
   have h_body := @let_binding_int_op_type_preservation 1000 "x" "x" "out" σ Δ Γ
               Ast.IntegerOp.add (Ast.Expr.constBool True) (Ast.Expr.constBool True) hΓ hΓ
