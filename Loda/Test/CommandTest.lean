f635d76bf5546d28a49e7ba5c9e0e8853559a327
Koukyosyumei <koukyosyumei@hotmail.com>
2025-06-21 17:34:07 +0900

    update

--- Loda/Test/CommandTest.lean
+++ Loda/Test/CommandTest.lean
@@ -11,12 +11,14 @@ open Env
 #loda_check Adder
 #loda_eval Adder x=2
 
+
 #loda_prove Adder := by {
   rename_i Δ h_delta x hs envs σ Γ hσ
-  have hΓ : Γ "x" = (Ty.int.refin (Expr.constBool True)) := rfl
+  have hΓ : Γ "x" = (Ty.int.refin (Ast.Predicate.const (Expr.constBool True))) := rfl
   have h_body := @let_binding_int_op_type_preservation "x" "x" "out" σ Δ Γ
-              Ast.IntegerOp.add (Ast.Expr.constBool True) (Ast.Expr.constBool True) hΓ hΓ
-  obtain ⟨vv, hv_eq⟩ := int_refintype_implies_exists_int_value σ Δ Γ "x" (Expr.constBool True) hΓ hσ
+              Ast.IntegerOp.add (Ast.Predicate.const (Ast.Expr.constBool True))
+                                (Ast.Predicate.const (Ast.Expr.constBool True)) hΓ hΓ
+  obtain ⟨vv, hv_eq⟩ := int_refintype_implies_exists_int_value σ Δ Γ "x" (Ast.Predicate.const (Ast.Expr.constBool True))  hΓ hσ
   have h_sub := two_mul_int σ Δ Γ "x" vv hv_eq
   simp [h_delta, Γ, Ast.v] at h_sub h_body
   exact Ty.TypeJudgment.TE_SUB h_sub h_body
--- Loda/Test/TypingTest.lean
+++ Loda/Test/TypingTest.lean
@@ -4,7 +4,7 @@ import Loda.Gadget
 @[simp]
 def mulCircuit : Ast.Circuit := {
   name   := "mul",
-  inputs := ("x", Ast.Ty.refin Ast.Ty.int (Ast.Predicate.eq (Ast.Expr.constBool true))),
+  inputs := ("x", Ast.Ty.refin Ast.Ty.int (Ast.Predicate.const (Ast.Expr.constBool true))),
   output := ("out", Ast.Ty.refin (Ast.Ty.int) (Ast.Predicate.eq (Ast.Expr.intExpr (Ast.Expr.constInt 2) Ast.IntegerOp.mul (Ast.Expr.var "x")))),
   body   := (Ast.Expr.letIn "out" (Ast.Expr.intExpr (Ast.Expr.var "x") Ast.IntegerOp.add (Ast.Expr.var "x")) (Ast.Expr.var "out"))
 }
@@ -27,11 +27,11 @@ theorem mulCircuit_correct : (Ty.circuitCorrect Δ mulCircuit) := by
   set envs := Ty.makeEnvs mulCircuit x
   set σ := envs.1
   set Γ := envs.2
-  have hΓ : Γ "x" = (Ast.Ty.int.refin (Ast.Predicate.eq (Ast.Expr.constBool true))) := rfl
+  have hΓ : Γ "x" = (Ast.Ty.int.refin (Ast.Predicate.const (Ast.Expr.constBool true))) := rfl
   have h_body := @let_binding_int_op_type_preservation "x" "x" "out" σ Δ Γ
-              Ast.IntegerOp.add (Ast.Predicate.eq (Ast.Expr.constBool true))
-                                (Ast.Predicate.eq (Ast.Expr.constBool true)) hΓ hΓ
-  obtain ⟨vv, hv_eq⟩ := int_refintype_implies_exists_int_value σ Δ Γ "x" (Ast.Predicate.eq (Ast.Expr.constBool true)) hΓ hσ
+              Ast.IntegerOp.add (Ast.Predicate.const (Ast.Expr.constBool true))
+                                (Ast.Predicate.const (Ast.Expr.constBool true)) hΓ hΓ
+  obtain ⟨vv, hv_eq⟩ := int_refintype_implies_exists_int_value σ Δ Γ "x" (Ast.Predicate.const (Ast.Expr.constBool true)) hΓ hσ
   have h_sub := two_mul_int σ Δ Γ "x" vv hv_eq
   exact Ty.TypeJudgment.TE_SUB h_sub h_body
 
