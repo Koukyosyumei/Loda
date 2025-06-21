59c7ade0b075b2e6d789d4102ec027598f73d474
Koukyosyumei <koukyosyumei@hotmail.com>
2025-06-21 17:22:26 +0900

    update

--- Loda/Test/TypingTest.lean
+++ Loda/Test/TypingTest.lean
@@ -4,16 +4,16 @@ import Loda.Gadget
 @[simp]
 def mulCircuit : Ast.Circuit := {
   name   := "mul",
-  inputs := ("x", Ast.Ty.refin Ast.Ty.int (Ast.Expr.constBool true)),
-  output := ("out", Ast.Ty.refin (Ast.Ty.int) (Ast.exprEq Ast.v (Ast.Expr.intExpr (Ast.Expr.constInt 2) Ast.IntegerOp.mul (Ast.Expr.var "x")))),
+  inputs := ("x", Ast.Ty.refin Ast.Ty.int (Ast.Predicate.eq (Ast.Expr.constBool true))),
+  output := ("out", Ast.Ty.refin (Ast.Ty.int) (Ast.Predicate.eq (Ast.Expr.intExpr (Ast.Expr.constInt 2) Ast.IntegerOp.mul (Ast.Expr.var "x")))),
   body   := (Ast.Expr.letIn "out" (Ast.Expr.intExpr (Ast.Expr.var "x") Ast.IntegerOp.add (Ast.Expr.var "x")) (Ast.Expr.var "out"))
 }
 
 @[simp]
 def addOneCircuit : Ast.Circuit := {
   name   := "assert1",
-  inputs := ("x", Ast.Ty.refin (Ast.Ty.field 5) (Ast.exprEq Ast.v (Ast.Expr.constF 5 1))),
-  output := ("out", Ast.Ty.refin (Ast.Ty.field 5) (Ast.exprEq Ast.v (Ast.Expr.fieldExpr (Ast.Expr.constF 5 1) Ast.FieldOp.add (Ast.Expr.constF 5 1)))),
+  inputs := ("x", Ast.Ty.refin (Ast.Ty.field 5) (Ast.Predicate.eq (Ast.Expr.constF 5 1))),
+  output := ("out", Ast.Ty.refin (Ast.Ty.field 5) (Ast.Predicate.eq (Ast.Expr.fieldExpr (Ast.Expr.constF 5 1) Ast.FieldOp.add (Ast.Expr.constF 5 1)))),
   body   := (Ast.Expr.letIn "out" (Ast.Expr.fieldExpr (Ast.Expr.var "x") Ast.FieldOp.add (Ast.Expr.var "x")) (Ast.Expr.var "out"))
 }
 
@@ -27,13 +27,14 @@ theorem mulCircuit_correct : (Ty.circuitCorrect Δ mulCircuit) := by
   set envs := Ty.makeEnvs mulCircuit x
   set σ := envs.1
   set Γ := envs.2
-  have hΓ : Γ "x" = (Ast.Ty.int.refin (Ast.Expr.constBool true)) := rfl
+  have hΓ : Γ "x" = (Ast.Ty.int.refin (Ast.Predicate.eq (Ast.Expr.constBool true))) := rfl
   have h_body := @let_binding_int_op_type_preservation "x" "x" "out" σ Δ Γ
-              Ast.IntegerOp.add (Ast.Expr.constBool true) (Ast.Expr.constBool true) hΓ hΓ
-  obtain ⟨vv, hv_eq⟩ := int_refintype_implies_exists_int_value σ Δ Γ "x" (Ast.Expr.constBool true) hΓ hσ
+              Ast.IntegerOp.add (Ast.Predicate.eq (Ast.Expr.constBool true))
+                                (Ast.Predicate.eq (Ast.Expr.constBool true)) hΓ hΓ
+  obtain ⟨vv, hv_eq⟩ := int_refintype_implies_exists_int_value σ Δ Γ "x" (Ast.Predicate.eq (Ast.Expr.constBool true)) hΓ hσ
   have h_sub := two_mul_int σ Δ Γ "x" vv hv_eq
   exact Ty.TypeJudgment.TE_SUB h_sub h_body
 
 def σ : Env.ValEnv := [("x", Ast.Value.vInt 5)]
-def Γ : Env.TyEnv := fun _ => Ast.Ty.refin Ast.Ty.int (Ast.Expr.constBool true)
+def Γ : Env.TyEnv := fun _ => Ast.Ty.refin Ast.Ty.int (Ast.Predicate.eq (Ast.Expr.constBool true))
 #eval Eval.eval σ Δ mulCircuit.body
